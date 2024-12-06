76eeba7af18cbea5c468e001d93a0cb6bb8fad26
Natasha Bryant <97158331+Tash91@users.noreply.github.com>
2024-12-06 14:50:59 +0000

    need to fix alter error on alembic migration

--- coloprevent/model/__init__.py
+++ coloprevent/model/__init__.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String
+from sqlalchemy import String, Integer
 from lbrc_flask.database import db
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from datetime import date
@@ -9,11 +9,28 @@ from datetime import date
 class Site(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True) 
     name: Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
+    back_up_contact:Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
+    site_primary_contact :Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
+    site_code:Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
 
 class PackTypes(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True) 
-    name: Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    
 
+
+class PackShipments(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    pack_ids: Mapped[int] = mapped_column(nullable=False, unique=True)
+    pack_expiry:Mapped[date] = mapped_column(nullable=False)
+    addressee:Mapped[str] = mapped_column(String(100), nullable=False)
+    date_posted:Mapped[date] = mapped_column( nullable=False)
+    date_recieved:Mapped[date] = mapped_column( nullable=False)
+    next_due:Mapped[date] = mapped_column( nullable=False)
+
+class PackId(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    pack_ids: Mapped[int] = mapped_column(nullable=False, unique=True)
 
 
     
@@ -23,9 +40,3 @@ class Packs(db.Model):
     expiry:Mapped[date]= mapped_column
    
     
-
-
-# class Consumables(db.Model):
-#     id:Mapped[int] = mapped_column(primary_key=True)
-#     consumable_type:Mapped[str] =mapped_column
-
