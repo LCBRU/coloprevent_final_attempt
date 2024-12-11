578e3170451c6a15844092f4ba30f7549045fdd6
Natasha Bryant <97158331+Tash91@users.noreply.github.com>
2024-12-11 15:42:32 +0000

    attempting radio field rendering change

--- coloprevent/ui/views/pack_types.py
+++ coloprevent/ui/views/pack_types.py
@@ -4,7 +4,7 @@ from lbrc_flask.forms import SearchForm
 from lbrc_flask.database import db
 from sqlalchemy import select
 from lbrc_flask.security import User
-from wtforms import HiddenField, StringField, RadioField
+from wtforms import HiddenField, StringField, RadioField, widgets
 from wtforms.validators import Length, DataRequired
 from lbrc_flask.forms import FlashingForm
 from lbrc_flask.response import refresh_response
@@ -12,10 +12,11 @@ from coloprevent.model import Site
 from flask_wtf import FlaskForm
 
 class PacktypeForm(FlaskForm):
-    pack_type_name = RadioField(u'Packtypes', choices=[('screening', 'Screening'), ('fit', 'FIT'), ('research', 'Research'), ('tissue_ffpe', 'Tissue/FFPE')])
+    pack_type_name = RadioField(u'Packtypes',choices=[('screening', 'Screening'), ('fit', 'FIT'), ('research', 'Research'), ('tissue_ffpe', 'Tissue/FFPE')])
 
 @blueprint.route('/packtypes', methods=['GET', 'POST'])
 def packtypes_home():
      packtypes_form = PacktypeForm()
      return render_template('ui/packtypes/packtypes_home.html',packtypes_form=packtypes_form)
+
 
