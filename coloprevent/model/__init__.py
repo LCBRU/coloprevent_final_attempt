b5908ea9eb15e52a2f95fb28220991c595ef673e
Natasha Bryant <97158331+Tash91@users.noreply.github.com>
2025-01-06 08:14:22 +0000

    attempting to add foreign keys and alter parent table

--- coloprevent/model/__init__.py
+++ coloprevent/model/__init__.py
@@ -12,6 +12,7 @@ class Site(db.Model):
     back_up_contact:Mapped[str] = mapped_column(String(100),nullable=False, unique=True)
     site_primary_contact: Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
     site_code:Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
+    pack_shipments: Mapped[list["PackShipments"]] = relationship(back_populates="site_id") #adding back refernce to packshipments connection
 
 
 class PackTypes(db.Model):
@@ -22,12 +23,15 @@ class PackTypes(db.Model):
 
 class PackShipments(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
+    site_id = Mapped[int] = mapped_column(ForeignKey("site.id")) #attempting to add site fk
     pack_ids: Mapped[int] = mapped_column(nullable=False, unique=True)
     pack_expiry:Mapped[date] = mapped_column(nullable=False)
     addressee:Mapped[str] = mapped_column(String(100), nullable=False)
     date_posted:Mapped[date] = mapped_column( nullable=False)
     date_recieved:Mapped[date] = mapped_column( nullable=False)
     next_due:Mapped[date] = mapped_column( nullable=False)
+    site: Mapped["Site"] = relationship(back_populates="pack_shipments") #adding backreference to site connection 
+
 
 
     
