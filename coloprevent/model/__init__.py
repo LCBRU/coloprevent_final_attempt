73567a053eec329e23aaea82802894f518747412
Natasha Bryant <97158331+Tash91@users.noreply.github.com>
2025-01-10 15:33:09 +0000

    edit db tables and create relationships between tables

--- coloprevent/model/__init__.py
+++ coloprevent/model/__init__.py
@@ -12,14 +12,15 @@ class Site(db.Model):
     site_back_up_contact:Mapped[str] = mapped_column(String(100),nullable=False, unique=True)
     site_primary_contact: Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
     site_code:Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
-    pack_shipments: Mapped[list["PackShipments"]] = relationship(back_populates="site_id") 
-    packs: Mapped[list['Packs']]= relationship(back_populates='site')
+    pack_shipments: Mapped[list["PackShipments"]] = relationship(back_populates="site") 
+    packs: Mapped[list["Packs"]]= relationship(back_populates='site')
 
 
 class PackTypes(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True) 
     packtype_name: Mapped[str] = mapped_column(nullable=False)
-    packs:Mapped[list["Packs"]] = relationship(back_populates="pack_types") 
+    packs:Mapped[list["Packs"]] = relationship(back_populates="packtypes") 
+
    
     
 class Packs(db.Model):
@@ -27,21 +28,22 @@ class Packs(db.Model):
     pack_identity:Mapped[int] = mapped_column(nullable=False)
     pack_expiry:Mapped[date]= mapped_column(nullable=False)
     pack_quantity: Mapped[int] = mapped_column(nullable=False)
-    pack_types: Mapped["PackTypes"] = relationship(back_populates="packs")
-    pack_types_id:Mapped[int] = mapped_column(ForeignKey("packtypes.id"))
     site: Mapped['Site']=relationship(back_populates='packs')
     site_id: Mapped[int]= mapped_column(ForeignKey("site.id"))
+    packtypes: Mapped['PackTypes']=relationship(back_populates=('packs'))
+    packtypes_id:Mapped[int]=mapped_column(ForeignKey("pack_types.id"))
+  
     
 
 
 class PackShipments(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    site_id: Mapped[int] = mapped_column(ForeignKey("site.id")) 
     addressee:Mapped[str] = mapped_column(String(100), nullable=False)
     date_posted:Mapped[date] = mapped_column( nullable=False)
     date_recieved:Mapped[date] = mapped_column( nullable=False)
     next_due:Mapped[date] = mapped_column( nullable=False)
     site: Mapped["Site"] = relationship(back_populates="pack_shipments") 
+    site_id: Mapped[int] = mapped_column(ForeignKey("site.id")) 
 
 
 
