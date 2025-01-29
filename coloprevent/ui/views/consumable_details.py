58e335a4f1c8a47634fbff4f2e8e7b94cd21ff4d
Natasha Bryant <97158331+Tash91@users.noreply.github.com>
2025-01-29 12:43:10 +0000

    create consumable details views and html pages

--- coloprevent/ui/views/consumable_details.py
+++ coloprevent/ui/views/consumable_details.py
@@ -23,7 +23,7 @@ def consumable_details_home():
 
     if search_form.search.data:
         q = q.where(ConsumableDetails.cat_no.like(f'%{search_form.search.data}%'))
-    return render_template('ui/consumables/consumable_details_home.html', ordered_list = ordered_list, search_form=search_form)
+    return render_template('ui/consumable_details/consumable_details_home.html', ordered_list = ordered_list, search_form=search_form)
 
 class ConsumableDetailsForm(FlaskForm):
     cat_no = StringField('Cat no', validators=[DataRequired()])
@@ -36,6 +36,82 @@ class ConsumableDetailsForm(FlaskForm):
     def __init__(self,  **kwargs):
         super().__init__(**kwargs)
 
-        self.consumable=[(c.id) for c in db.session.execute(select(Consumable)).scalars()]
+        self.consumable.choices=[(c.id, c.consumable_name) for c in db.session.execute(select(Consumable)).scalars()]
+
+
+@blueprint.route('/add_consumable_details', methods=['GET', 'POST'])
+def add_consumable_details ():
+    consumable_details_form = ConsumableDetailsForm()
+    if consumable_details_form.validate_on_submit():
+        consumable_details_added = ConsumableDetails(
+        cat_no = consumable_details_form.cat_no.data,
+        supplier = consumable_details_form.supplier.data,
+        price_increase = consumable_details_form.price_increase.data,
+        price = consumable_details_form.price.data,
+        quantity_per_pack = consumable_details_form.quantity_per_pack.data,
+        cons_name_id = consumable_details_form.consumable.data
+        )
+        db.session.add(consumable_details_added)
+        db.session.commit()
+        return refresh_response()
     
+    return render_template('lbrc/form_modal.html', form=consumable_details_form, title="Add Consumable details", url=url_for("ui.add_consumable_details"))
+    
+
+@blueprint.route('/delete_consumable_details/<int:id>', methods=['GET', 'POST'])
+def delete_consumable_details(id):
+    delete_id = id
+    if id== delete_id:
+        query_del = db.session.execute(db.select(ConsumableDetails).where(ConsumableDetails.id == delete_id)).scalar()
+        db.session.delete(query_del)
+        db.session.commit()
+        return redirect(url_for('ui.consumable_details_home'))
+    return render_template('ui/consumable_details/delete_consumable_details.html', id=id)
+
+
+@blueprint.route('/edit_consumable_details/<int:id>', methods=['GET', 'POST'])
+def edit_consumable_details(id):
+    edit_id = id
+    if id== edit_id:
+        query_edit = db.session.execute(db.select(ConsumableDetails).where(ConsumableDetails.id == edit_id)).scalar()
+        prev_cat_no = query_edit.cat_no
+        prev_supplier = query_edit.supplier
+        prev_price_increase = query_edit.price_increase
+        prev_price = query_edit.price
+        prev_quantity_per_pack = query_edit.quantity_per_pack
+        prev_consumable= query_edit.cons_name_id
+
+
+        ed_form=ConsumableDetailsForm(cat_no=prev_cat_no, supplier=prev_supplier, price_increase=prev_price_increase, price=prev_price,
+                                       quantity_per_pack=prev_quantity_per_pack, consumable=prev_consumable) 
+
+    
+    if ed_form.validate_on_submit():
+            query_edit.cat_no = ed_form.cat_no.data
+            query_edit.supplier = ed_form.supplier.data
+            query_edit.price_increase = ed_form.price_increase.data
+            query_edit.price = ed_form.price.data
+            query_edit.quantity_per_pack = ed_form.quantity_per_pack.data
+            query_edit.cons_name_id = ed_form.consumable.data 
+            db.session.add(query_edit)
+            db.session.commit()
+            return refresh_response()
+        
+
+    return render_template('lbrc/form_modal.html', form = ed_form, id=id, title="Edit Consumable details", url=url_for("ui.edit_consumable_details",id=id))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
