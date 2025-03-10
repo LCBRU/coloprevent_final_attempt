4916da9efb6e91cf609ca94802866f6b621d93cc
Natasha Bryant <97158331+Tash91@users.noreply.github.com>
2025-03-10 14:48:13 +0000

    add packtype to report

--- coloprevent/ui/views/pack_exp_report.py
+++ coloprevent/ui/views/pack_exp_report.py
@@ -2,12 +2,12 @@ from .. import blueprint
 from flask import render_template, request, url_for, redirect
 from lbrc_flask.forms import SearchForm
 from lbrc_flask.database import db
-from sqlalchemy import select
+from sqlalchemy import select, and_
 from lbrc_flask.security import User
 from wtforms.validators import Length, DataRequired
 from lbrc_flask.forms import FlashingForm, SearchForm
 from lbrc_flask.response import refresh_response
-from coloprevent.model import  Pack, Site, PackShipment
+from coloprevent.model import  Pack, Site, PackShipment, PackType
 from flask_wtf import FlaskForm
 from lbrc_flask.requests import get_value_from_all_arguments
 
@@ -17,12 +17,12 @@ def pack_expiry_report():
    q = select(
    Pack.pack_identity,
    Pack.pack_expiry,
+   PackType.packtype_name,
    Site.site_name,
    ).join(
    Pack.pack_shipment
    ).join(
-   PackShipment.site,
-   )
+   PackShipment.site)
 
 
 
