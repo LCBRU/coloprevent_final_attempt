410e031efe022804a44431627f58336e0e6a53ee
Natasha Bryant <97158331+Tash91@users.noreply.github.com>
2025-03-27 09:36:10 +0000

    fix alert status to only show if packs are 30 days old

--- coloprevent/ui/views/test_alert.py
+++ coloprevent/ui/views/test_alert.py
@@ -1,10 +1,35 @@
 from .. import blueprint
 from flask import render_template
-#from coloprevent.model import  Pack, Site, PackShipment
+from lbrc_flask.database import db
+from coloprevent.model import  Pack
+from sqlalchemy import select, desc
+from datetime import *
+
+
+today = date.today()
+print(today)
+
+expiry_alert_list= []
+
+
+
+print(f" today is {today}")
 
 @blueprint.route("/is_updating")
 def is_updating():
-    count=5
-    return render_template("ui/alerts/updating.html", count=count)
+    expiry_alert = False
+    q = db.select(Pack.pack_expiry).order_by(Pack.pack_expiry)
+    q_date = db.session.execute(q).scalars()
+    for dat_dif in q_date:
+        calc_diff= (today - dat_dif).days
+        print(calc_diff)
+        print(type(calc_diff))
+        if calc_diff >= 30:
+            expiry_alert_list.append(calc_diff)
+    if len(expiry_alert_list) >= 1:
+        expiry_alert = True
+          
+    
+    return render_template("ui/alerts/updating.html", expiry_alert=expiry_alert)
 
 
