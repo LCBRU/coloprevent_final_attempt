fe5761aeb08d818f0518545fd3aad23acec30f30
Richard Bramley <rabramley@gmail.com>
2025-06-03 09:13:10 +0100

    Refactor pack expiry report download

--- coloprevent/ui/views/pack_exp_report.py
+++ coloprevent/ui/views/pack_exp_report.py
@@ -20,77 +20,69 @@ class DropDownForm (SearchForm):
     pack_expriry_to = DateField()
     site = SelectField ('Site')
 
-
     def __init__(self,  **kwargs):
         super().__init__(**kwargs)
 
         self.packtype.choices=[("","")]+[(pk.id, pk.packtype_name) for pk in db.session.execute(select(PackType)).scalars()]
-
         self.site.choices=[("","")]+[(s.id, s.site_name) for s in db.session.execute(select(Site)).scalars()]
 
 
-@blueprint.route('/pack_expiry_report', methods=['GET', 'POST'])
+@blueprint.route('/pack_expiry_report')
 def pack_expiry_report():
-   search_form = DropDownForm(search_placeholder='Search Pack ID', formdata=request.args) 
-   q = db.select(        #added db.
-   Pack.pack_identity,
-   Pack.pack_expiry,
-   PackType.packtype_name,
-   Site.site_name,
-   ).join(Pack.packtype
-   ).join(
-   Pack.pack_shipment
-   ).join(
-   PackShipment.site)
-
-
-
-
-   if search_form.search.data:
-      q = q.where(Pack.pack_identity == search_form.search.data)
-
-   if search_form.packtype.data:
-      q = q.where(PackType.id == search_form.packtype.data)
+   search_form = DropDownForm(search_placeholder='Search Pack ID', formdata=request.args)
+   q = get_pack_expiry_report_query(search_form)
    
-   if search_form.pack_expriry_from.data:
-      q = q.where(Pack.pack_expiry > search_form.pack_expriry_from.data)
-
-   if search_form.pack_expriry_to.data:
-      q = q.where(Pack.pack_expiry < search_form.pack_expriry_to.data)
-
-   if search_form.site.data:
-      q = q.where(PackShipment.site_id == search_form.site.data)
-
-  
-   
-   q_download = db.session.execute(q).all()
-
-
-
-   if request.method == "POST": #attempting csv code 
-      download_button = request.form.get('download_report')
-      if download_button is not None:  
-         with open ('expiry_csv', 'w', newline='') as csvfile:
-            csvwriter = csv.writer(csvfile , delimiter=',')
-            csvwriter.writerow(["Pack Identity", "Packtype", "Pack Expiry", "Site"])
-            for q_line in q_download:
-               csvwriter.writerow([q_line.pack_identity, q_line.packtype_name, q_line.pack_expiry, q_line.site_name])
-
-         return send_file('../expiry_csv',
-                        mimetype='text/csv',
-                        as_attachment=True,
-                        download_name="Expiry_report.csv"
-
-                        )
-
-      
- 
-
-
-
-
    results = db.session.execute(q).mappings()
    return render_template( "ui/reports/pack_exp_report.html", results=results, search_form=search_form)
+
+
+@blueprint.route('/pack_expiry_report_download')
+def pack_expiry_report_download():
+   search_form = DropDownForm(formdata=request.args)
+   q = get_pack_expiry_report_query(search_form)
+   
+   packs = db.session.execute(q).all()
+
+   with open ('expiry_csv', 'w', newline='') as csvfile:
+      csvwriter = csv.writer(csvfile , delimiter=',')
+      csvwriter.writerow(["Pack Identity", "Packtype", "Pack Expiry", "Site"])
+      for q_line in packs:
+         csvwriter.writerow([q_line.pack_identity, q_line.packtype_name, q_line.pack_expiry, q_line.site_name])
+
+   return send_file('../expiry_csv',
+      mimetype='text/csv',
+      as_attachment=True,
+      download_name="Expiry_report.csv"
+   )
+
+
+def get_pack_expiry_report_query(search_form):
+    q = db.select(        #added db.
+      Pack.pack_identity,
+      Pack.pack_expiry,
+      PackType.packtype_name,
+      Site.site_name,
+   ).join(Pack.packtype
+   ).join(
+      Pack.pack_shipment
+   ).join(
+      PackShipment.site)
+
+    if search_form.search.data:
+       q = q.where(Pack.pack_identity == search_form.search.data)
+
+    if search_form.packtype.data:
+       q = q.where(PackType.id == search_form.packtype.data)
+   
+    if search_form.pack_expriry_from.data:
+       q = q.where(Pack.pack_expiry > search_form.pack_expriry_from.data)
+
+    if search_form.pack_expriry_to.data:
+       q = q.where(Pack.pack_expiry < search_form.pack_expriry_to.data)
+
+    if search_form.site.data:
+       q = q.where(PackShipment.site_id == search_form.site.data)
+    return q
   
 
 
